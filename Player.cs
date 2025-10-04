931e493c94102ca73e48cc18e7d939f572615bb7
thaud0000 <thaud3967@gmail.com>
2025-10-04 20:16:13 +0900

    [INIT]First commit
    기초 클래스만 등록한 커밋

---
+++ Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game
+{
+    internal class Player
+    {
+        int playerHp;
+        int playerMp;
+        int playerAttack;
+        int playerDeffense;
+
+    }
+}
